47123ef856be41f1a7812fc9a70551bf6db7c80e
tushar-goyal-25 <pottertushar@gmail.com>
2024-04-13 22:24:03 +0100

    frontend and backend

--- ripple/src/pages/Bill.js
+++ ripple/src/pages/Bill.js
@@ -1,80 +1,3 @@
-// import React, { useState,useEffect } from 'react';
-// import './Bill.css';
-// import { useNavigate } from 'react-router-dom'
-
-// const Bill = () => {
-//     const [transactions, setTransactions] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch data from transactions.json
-//     fetch('bills.json')
-//       .then((response) => response.json())
-//       .then((data) => setTransactions(data))
-//       .catch((error) => console.error('Error fetching data:', error));
-//   }, []);
-
-// const navigate = useNavigate()
-
-
-//     return (
-//         <body>
-//         <header
-//           data-thq="thq-navbar"
-//           className="navbarContainer home1-navbar-interactive"
-//         >
-//           <span className="logo">BANKIFY</span>
-//           <div data-thq="thq-navbar-nav" className="home1-desktop-menu">
-//             <nav className="home1-links"></nav>
-//             <div className="home1-buttons">
-//             <button className="home1-login buttonFlat" onClick={() => navigate('/CreateBill')}>Create Bill</button>
-//             <button className="home1-login buttonFlat" onClick={() => navigate('/home')}>Home</button>
-//             <button className="home1-login buttonFlat" onClick={() => navigate('/AddSource')}>Add Account</button>
-//             <button className="home1-login buttonFlat" onClick={() => navigate('/payment')}>Transaction</button>
-//               <button className="home1-login buttonFlat" onClick={() => navigate('/')}>Logout</button>
-//             </div>
-//           </div>
-//           <div data-thq="thq-burger-menu" className="home1-burger-menu">
-           
-//           </div>
-//           <div
-//             data-thq="thq-mobile-menu"
-//             className="home1-mobile-menu1 mobileMenu"
-//           >
-             
-//           </div>
-//         </header>
-//         <table>
-//           <thead>
-//             <tr>
-//               <th>Sender Name</th>
-//               <th>Reference Name</th>
-//               <th>Amount</th>
-//               <th>Date</th>
-//               <th>Payment Due Date</th>
-//               <th>Actions</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {transactions.map((transaction) => (
-//               <tr key={transaction.id}>
-//                 <td>{transaction.senderName}</td>
-//                 <td>{transaction.referenceName}</td>
-//                 <td>${transaction.amount.toFixed(2)}</td>
-//                 <td>{transaction.date}</td>
-//                 <td>{transaction.paymentDueDate}</td>
-//                 <td>
-//                   <button className="payment-button">Pay</button>
-//                   <button className="receipt-button">See Receipt</button>
-//                 </td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//         </body>
-//       );
-//             }
-
-// export default Bill;
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Bill.css';
@@ -104,8 +27,8 @@ const Bill = () => {
                 <div data-thq="thq-navbar-nav" className="home1-desktop-menu">
                     <nav className="home1-links"></nav>
                     <div className="home1-buttons">
-                        <button className="home1-login buttonFlat" onClick={() => navigate('/CreateBill')}>Create Bill</button>
                         <button className="home1-login buttonFlat" onClick={() => navigate('/home')}>Home</button>
+                        <button className="home1-login buttonFlat" onClick={() => navigate('/CreateBill')}>Create Bill</button>
                         <button className="home1-login buttonFlat" onClick={() => navigate('/AddSource')}>Add Account</button>
                         <button className="home1-login buttonFlat" onClick={() => navigate('/payment')}>Transaction</button>
                         <button className="home1-login buttonFlat" onClick={() => navigate('/')}>Logout</button>
--- ripple/src/pages/Home.js
+++ ripple/src/pages/Home.js
@@ -1,105 +1,151 @@
-import React from 'react'
-// import { routes } from '../routes'
-import { Helmet } from 'react-helmet'
-import { useNavigate } from 'react-router-dom'
-// import FeatureCard from '../components/feature-card'
-import './Home.css'
-// import './style.css'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import './Home.css';
+import { useNavigate } from 'react-router-dom';
 
+const Home = () => {
+    const [bankAccounts, setBankAccounts] = useState([]);
+    const [cryptoAccounts, setCryptoAccounts] = useState([]);
+    const [microFinanceAccounts, setMicroFinanceAccounts] = useState([]);
+    const navigate = useNavigate();
 
-const Home= (props) => {
-  const navigate = useNavigate()
-  return (
-    <div className="home1-container">
-      <Helmet>
-        <title>Home</title>
-        <meta
-          property="og:title"
-          content="Home"
-        />
-      </Helmet>
-      <header
-          data-thq="thq-navbar"
-          className="navbarContainer home1-navbar-interactive"
-        >
-          <span className="logo">BANKIFY</span>
-          <div data-thq="thq-navbar-nav" className="home1-desktop-menu">
-            <nav className="home1-links"></nav>
-            <div className="home1-buttons">
-            <button className="home1-login buttonFlat" onClick={() => navigate('/CreateBill')}>Create Bill</button>
-            <button className="home1-login buttonFlat" onClick={() => navigate('/Bill')}>My Bills</button>
-            <button className="home1-login buttonFlat" onClick={() => navigate('/AddSource')}>Add Account</button>
-            <button className="home1-login buttonFlat" onClick={() => navigate('/home')}>Home</button>
-              <button className="home1-login buttonFlat" onClick={() => navigate('/')}>Logout</button>
-            </div>
-          </div>
-          <div data-thq="thq-burger-menu" className="home1-burger-menu">
-           
-          </div>
-          <div
-            data-thq="thq-mobile-menu"
-            className="home1-mobile-menu1 mobileMenu"
-          >
-             
-          </div>
-        </header>
-      <div className="home1-hero">
-        <div className="home1-hero1 heroContainer">
-        <div className="account-boxes">
-                        <div className="account-box">Acc1</div>
-                        <div className="account-box">Acc2</div>
-                        <div className="account-box">Acc3</div>
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/getaccount/')
+            .then(response => {
+                const data = response.data;
+                const bank = data.filter(account => account.account_type === 'Bank');
+                const crypto = data.filter(account => account.account_type === 'Crypto');
+                const microFinance = data.filter(account => account.account_type === 'Micro-Finance');
+
+                setBankAccounts(bank);
+                setCryptoAccounts(crypto);
+                setMicroFinanceAccounts(microFinance);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return (
+        <div className="home1-container">
+            <header
+                data-thq="thq-navbar"
+                className="navbarContainer home1-navbar-interactive"
+            >
+                <span className="logo">BANKIFY</span>
+                <div data-thq="thq-navbar-nav" className="home1-desktop-menu">
+                    <nav className="home1-links"></nav>
+                    <div className="home1-buttons">
+                        <button className="home1-login buttonFlat" onClick={() => navigate('/CreateBill')}>Create Bill</button>
+                        <button className="home1-login buttonFlat" onClick={() => navigate('/Bill')}>My Bills</button>
+                        <button className="home1-login buttonFlat" onClick={() => navigate('/AddSource')}>Add Account</button>
+                        <button className="home1-login buttonFlat" onClick={() => navigate('/payment')}>Transaction</button>
+                        <button className="home1-login buttonFlat" onClick={() => navigate('/')}>Logout</button>
                     </div>
-          <div className="home1-container1">
-
-          
-            <h2 className="heading2"> Transaction History</h2>
-            <div className="home1-container2">
-              
-              <div className="home1-btn-group"></div>
+                </div>
+                <div data-thq="thq-burger-menu" className="home1-burger-menu"></div>
+                <div data-thq="thq-mobile-menu" className="home1-mobile-menu1 mobileMenu"></div>
+            </header>
+            <div className="account-boxes">
+                <div className="account-box">
+                    <h3>Bank Accounts</h3>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Account Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {bankAccounts.map(account => (
+                                <tr key={account.id}>
+                                    <td>{account.account_name}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <div className="account-box">
+                    <h3>Crypto Accounts</h3>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Account Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cryptoAccounts.map(account => (
+                                <tr key={account.id}>
+                                    <td>{account.account_name}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <div className="account-box">
+                    <h3>Micro-Finance Accounts</h3>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Account Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {microFinanceAccounts.map(account => (
+                                <tr key={account.id}>
+                                    <td>{account.account_name}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-          </div>
-          <div class="transaction-history">
-    <table>
-        <thead>
-            <tr>
-                <th>Time</th>
-                <th>Name of Receiver</th>
-                <th>Amount</th>
-                <th>Transaction Reference</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>2024-04-13 10:30 AM</td>
-                <td>John Doe</td>
-                <td>$100.00</td>
-                <td>Term 1</td>
-            </tr>
-            <tr>
-                <td>2024-04-12 02:45 PM</td>
-                <td>Jane Doe</td>
-                <td>$50.00</td>
-                <td>Term 2</td>
-            </tr>
-        </tbody>
-    </table>
-    
-</div>
-
+            <div className="home1-hero">
+                <div className="home1-hero1 heroContainer">
+                    <div className="home1-container1">
+                        <h2 className="heading2"> Transaction History</h2>
+                        <div className="home1-container2">
+                            <div className="home1-btn-group"></div>
+                        </div>
+                    </div>
+                    <div className="transaction-history">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Time</th>
+                                    <th>Name of Receiver</th>
+                                    <th>Amount</th>
+                                    <th>Transaction Reference</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>2024-04-13 10:30 AM</td>
+                                    <td>John Doe</td>
+                                    <td>$100.00</td>
+                                    <td>Term 1</td>
+                                </tr>
+                                <tr>
+                                    <td>2024-04-12 02:45 PM</td>
+                                    <td>Jane Doe</td>
+                                    <td>$50.00</td>
+                                    <td>Term 2</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div className="home1-features">
+                <div className="home1-features-container featuresContainer"></div>
+            </div>
+            <div className="home1-pricing"></div>
+            <div className="home1-banner"></div>
+            <div className="home1-faq">
+                <div className="home1-faq-container faqContainer"></div>
+            </div>
+            <div className="home1-footer"></div>
         </div>
-      </div>
-      <div className="home1-features">
-        <div className="home1-features-container featuresContainer"></div>
-      </div>
-      <div className="home1-pricing"></div>
-      <div className="home1-banner"></div>
-      <div className="home1-faq">
-        <div className="home1-faq-container faqContainer"></div>
-      </div>
-      <div className="home1-footer"></div>
-    </div>
-  )
-}
+    );
+};
 
-export default Home
+export default Home;
